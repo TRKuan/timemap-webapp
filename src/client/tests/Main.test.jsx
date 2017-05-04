78fd6814b2a277116169622d51334da1f5d864f9
TR <tianruei.kuan@gmail.com>
2017-05-04 16:47:31 +0800

    Add test

--- src/client/tests/Main.test.jsx
+++ src/client/tests/Main.test.jsx
@@ -1,8 +1,12 @@
 var expect = require('expect');
+import Main from 'components/Main.jsx';
 
 // describe just breaks the tests in groups
 describe('Main', () => {
-  it('should properly run tests', () =>{
-      expect(1).toBe(1);
-  });
+    it('should properly run tests', () =>{
+        expect(1).toBe(1);
+    });
+    it('should exist', () => {
+        expect(Main).toExist();
+    });
 });
