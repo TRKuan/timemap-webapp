619a0356be528f40f68d235ac4f44d9b22110353
TR <tianruei.kuan@gmail.com>
2017-05-04 16:48:01 +0800

    Move redux store to index.jsx

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
+
 import './Main.css';
 
-export default class Main extends React.Component {
+class Main extends React.Component {
     constructor(props) {
         super(props);
         this.store = null;
     }
 
-    componentWillMount() {
-        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-        this.store = createStore(combineReducers({
-            //TODO: reducers
-        }), composeEnhancers(applyMiddleware(thunkMiddleware)));
-    }
-
     render() {
         return (
-            <Provider store={this.store}>
-                <div className="main">
-                  <h1>Hello word</h1>
-                </div>
-            </Provider>
+            <div className="main">
+              <h1>Hello word</h1>
+            </div>
         );
     }
 }
+
+export default connect((state) => {
+    return {
+        ...state
+    };
+})(Main);
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {Provider} from 'react-redux';
 
 import Main from 'components/Main.jsx';
 
 import './index.css';
 
 window.onload = function() {
-    ReactDOM.render(
-        <Main/>,
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    this.store = createStore(combineReducers({
+        //TODO: reducers
+    }), composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+    ReactDOM.render((
+        <Provider store={this.store}>
+          <Main/>
+        </Provider>
+        ),
         document.getElementById('root')
     );
 };
