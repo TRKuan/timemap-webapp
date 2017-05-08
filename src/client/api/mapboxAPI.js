527f62a4dface43589cb5b112cdb4d34e97e1599
TR <tianruei.kuan@gmail.com>
2017-05-08 23:00:00 +0800

    Change getDuratuin to getDirection in mapboxAIP.js

--- src/client/api/mapboxAPI.js
+++ src/client/api/mapboxAPI.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-export function getDuration(coords1, coords2, profile, accessToken){
+export function getDirection(coords1, coords2, profile, accessToken){
     const directionURL = `https://api.mapbox.com/directions/v5/mapbox/${profile}/${coords1.lng},${coords1.lat};${coords2.lng},${coords2.lat}.json?access_token=${accessToken}`;
     axios.get(directionURL).then((res) => {
-        return res.data.routes[0].duration;
+        return {
+            duration: res.data.routes[0].duration,
+            distance: res.data.routes[0].distance
+        };
     }).
     catch((err) => {
         if (axios.isCancel(err)) {
