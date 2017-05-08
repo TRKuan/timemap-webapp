d5acc69e4b257112b5fa0e3e03689d09695994ba
TR <tianruei.kuan@gmail.com>
2017-05-08 23:44:37 +0800

    error handle

--- src/client/api/mapboxAPI.js
+++ src/client/api/mapboxAPI.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export function getDirection(coords1, coords2, profile, accessToken){
     const directionURL = `https://api.mapbox.com/directions/v5/mapbox/${profile}/${coords1.lng},${coords1.lat};${coords2.lng},${coords2.lat}.json?access_token=${accessToken}`;
     return axios.get(directionURL).then((res) => {
+        if(res.data.code !== "Ok")throw Error("Mapbox Direaction error");
         return {
             duration: res.data.routes[0].duration,
             distance: res.data.routes[0].distance
