759646c1cda085c516eb31a844d76dc808a5c6c7
TR <tianruei.kuan@gmail.com>
2017-05-13 00:22:19 +0800

    get accessToken from server

--- src/client/api/mapboxAPI.js
+++ src/client/api/mapboxAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+const baseUrl = 'http://localhost:8080/api';//url for server
 export function getDirection(coords1, coords2, profile, accessToken){
     const directionURL = `https://api.mapbox.com/directions/v5/mapbox/${profile}/${coords1.lng},${coords1.lat};${coords2.lng},${coords2.lat}.json?access_token=${accessToken}`;
     return axios.get(directionURL).then((res) => {
@@ -15,5 +15,18 @@ export function getDirection(coords1, coords2, profile, accessToken){
         } else {
             throw err;
         }
+    });
+}
+
+export function getAccessToken() {
+    let url = `${baseUrl}/accesstoken`;
+
+    console.log(`Making POST request to: ${url}`);
+
+    return axios.get(url).then((res) => {
+        if (res.status !== 200)
+            throw new Error(`Unexpected response code: ${res.status}`);
+
+        return res.data;
     });
 }
